33a765d70601b546af6a5b88c1ebe3547b4f0bc4
Michael Ojogbo <linkupxc@gmail.com>
2023-11-23 14:18:06 +0100

    creator

--- app/comonents/Footer.tsx
+++ app/comonents/Footer.tsx
@@ -1,6 +1,7 @@
 import { IoLogoInstagram } from "react-icons/io";
 import { FaXTwitter } from "react-icons/fa6";
 import { IoMailOutline } from "react-icons/io5";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -8,7 +9,7 @@ const Footer = () => {
     <div className="w-full md:border-t md:px-16 py-8 flex md:flex-row flex-col items-center justify-between gap-8">
         <h1>90S PLAYER &copy;</h1>
         <div className="flex items-center justify-center gap-16">
-            <h1>MICHAEL OJOGBO</h1>
+            <Link href='https://michaelojogbo.vercel.app'>MICHAEL OJOGBO</Link>
             <div className="flex items-center justify-center gap-8">
                 <IoLogoInstagram href="https://instagram.com/90splayer" target={"_blank"}/>
                 <FaXTwitter href="https://twitter.com/90splayer" target={"_blank"}/>
